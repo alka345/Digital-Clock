e774d719cb315a6a0c70b61929081367b5a44c74
alka345 <alkaraojnv@gmail.com>
2023-12-13 21:42:17 +0530

    code review

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const clock =document.getElementById("clock")
+const clock =document.getElementById("clock");
+
 setInterval(()=>{
     let d= new Date();
-    // console.log(d);
-    htime=d.getHours();
-    // console.log(htime);
-    mtime=d.getMinutes();
-    // console.log(mtime);
-    stime=d.getSeconds();
-    // console.log(stime);
+clock.innerHTML=d.toLocaleTimeString();
+
+    
+},1000);
+  // htime=d.getHours();
+    // // console.log(htime);
+    // mtime=d.getMinutes();
+    // // console.log(mtime);
+    // stime=d.getSeconds();
+    // // console.log(stime);
 
     //how to display it over digital clock
-    clock.innerHTML=`<h3>${htime}:${mtime}:${stime}</h3>`
+    // clock.innerHTML=`<h3>${htime}:${mtime}:${stime}</h3>`
     //continuosly run kre clock --used setInterval
-    
-},1000)
